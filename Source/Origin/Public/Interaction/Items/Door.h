a08557ba67803e9ba2f96f0c7f5d7184efcc5ec9
SauravCraft <Sauravkumar7739s@gmail.com>
2026-08-01 00:35:37 +0530

    Add Inreactable interface door

---
+++ Source/Origin/Public/Interaction/Items/Door.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interaction/Interface/Interactable.h"
+#include "Interaction/BaseActor/BaseActor.h"
+#include "Door.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ORIGIN_API ADoor : public ABaseActor,public IInteractable
+{
+	GENERATED_BODY()
+	
+
+public:
+	virtual void Interact() override;
+};
