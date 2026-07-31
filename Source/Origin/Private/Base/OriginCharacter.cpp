6f310a4ff01e8d2a85687c36a51d45b6bca1ad6f
SauravCraft <Sauravkumar7739s@gmail.com>
2026-08-01 01:38:07 +0530

    Interaction System Added Using Door Interface Actor

--- Source/Origin/Private/Base/OriginCharacter.cpp
+++ Source/Origin/Private/Base/OriginCharacter.cpp
@@ -1,5 +1,10 @@
 #include "Base/OriginCharacter.h"
 #include "Engine/CollisionProfile.h"
+#include "EnhancedInputSubsystems.h"
+#include "Interaction/Interface/Interactable.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+#include "EnhancedInputComponent.h"
 
 AOriginCharacter::AOriginCharacter()
 {
@@ -20,4 +25,110 @@ AOriginCharacter::AOriginCharacter()
 	// Optional
 	CapsuleComponent->SetGenerateOverlapEvents(true);
 	// CapsuleComponent->CanCharacterStepUpOn = ECB_Yes;
+}
+
+
+void AOriginCharacter::BeginPlay()
+{
+
+	Super::BeginPlay();
+
+	if (APlayerController* PC = Cast<APlayerController>(GetController()))
+	{
+		if (!PC) return;
+		ULocalPlayer* LP = PC->GetLocalPlayer();
+			if (!LP) return;
+			UEnhancedInputLocalPlayerSubsystem* Subsystem = LP->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>();
+			if (!Subsystem) return;
+			
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+
+}
+
+void AOriginCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	UEnhancedInputComponent* EnhancedInput =
+		Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	if (!EnhancedInput) return;
+
+	// Binding Input Function 
+	EnhancedInput->BindAction(
+		InteractAction,
+		ETriggerEvent::Started,
+		this,
+		&AOriginCharacter::Interaction);
+
+
+}
+
+
+void AOriginCharacter::Interaction(const FInputActionValue& Value)
+{
+    APlayerController* PC = Cast<APlayerController>(GetController());
+
+    if (!PC)
+    {
+        return;
+    }
+
+    int32 ViewportX, ViewportY;
+    PC->GetViewportSize(ViewportX, ViewportY);
+
+    FVector WorldLocation;
+    FVector WorldDirection;
+
+    PC->DeprojectScreenPositionToWorld(
+        ViewportX * 0.5f,
+        ViewportY * 0.5f,
+        WorldLocation,
+        WorldDirection
+    );
+
+    FVector Start = WorldLocation;
+    FVector End = Start + WorldDirection * 300.f;
+
+    FHitResult Hit;
+
+    FCollisionQueryParams Params;
+
+    Params.AddIgnoredActor(this);
+
+    bool bHit = GetWorld()->LineTraceSingleByChannel(
+        Hit,
+        Start,
+        End,
+        ECC_Visibility,
+        Params
+    );
+
+    DrawDebugLine(
+        GetWorld(),
+        Start,
+        End,
+        FColor::Red,
+        false,
+        2.f,
+        0,
+        1.f
+    );
+
+    if (!bHit)
+        return;
+
+    AActor* HitActor = Hit.GetActor();
+
+    if (!HitActor)
+        return;
+
+    IInteractable* Interactable =
+        Cast<IInteractable>(HitActor);
+
+    if (Interactable)
+    {
+        Interactable->Interact();
+    }
 }
--- Source/Origin/Public/Base/OriginCharacter.h
+++ Source/Origin/Public/Base/OriginCharacter.h
@@ -2,8 +2,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
 #include "Components/CapsuleComponent.h"
 #include "OriginCharacter.generated.h"
+
+
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS(Blueprintable)
 class ORIGIN_API AOriginCharacter : public APawn
@@ -18,4 +23,21 @@ protected:
 	/** Root Capsule */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	TObjectPtr<UCapsuleComponent> CapsuleComponent;
+
+
+	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Mapping Context
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputMappingContext* DefaultMappingContext;
+
+	// Input Actions 
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* InteractAction;
+
+	void Interaction(const FInputActionValue& Value);
+
+
 };
